c34325b2b4b1eb0beeab6370b450f4bfa7c6190d
Iulia Dumitru <iuliadmtru@gmail.com>
2025-12-07 00:50:12 +0100

    Fade away dead tree

--- scripts/tree.gd
+++ scripts/tree.gd
@@ -1,7 +1,7 @@
 extends Node2D
 class_name SnowyTree
 
-enum State {SNOWY, FALLING, DECORATED}
+enum State {SNOWY, FALLING, DECORATED, DEAD}
 
 @export var state: State = State.SNOWY
 @export var selected: bool = false
@@ -11,8 +11,16 @@ func _process(_delta):
 		State.SNOWY:
 			$Sprite.play("snowy")
 		State.FALLING:
-			$Sprite.play("falling")
+			$Sprite.play("falling", 0.75)
+			if $Sprite.frame == 3:
+				state = State.DEAD
 		State.DECORATED:
 			$Sprite.play("decorated")
+		State.DEAD:
+			fade()
 	
 	$Halo.visible = selected
+
+func fade():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Sprite, "modulate", Color.TRANSPARENT, 1.0)
