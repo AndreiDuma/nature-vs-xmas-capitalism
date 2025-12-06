8c03aee3398b9366e1824e6e794969d4b90f13d3
Andrei Duma <1846640+AndreiDuma@users.noreply.github.com>
2025-12-07 00:15:50 +0100

    New falling state for trees

--- scripts/tree.gd
+++ scripts/tree.gd
@@ -1,7 +1,7 @@
 extends Node2D
 class_name SnowyTree
 
-enum State {SNOWY, DECORATED}
+enum State {SNOWY, FALLING, DECORATED}
 
 @export var state: State = State.SNOWY
 @export var selected: bool = false
@@ -10,6 +10,8 @@ func _process(_delta):
 	match state:
 		State.SNOWY:
 			$Sprite.play("snowy")
+		State.FALLING:
+			$Sprite.play("falling")
 		State.DECORATED:
 			$Sprite.play("decorated")
 	
