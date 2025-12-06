9e1d874f3ddcf62a58c4ec139d1874da97e403e7
Iulia Dumitru <iuliadmtru@gmail.com>
2025-12-06 15:28:08 +0100

    Animate santa chopping; update santa standing

--- scenes/animated-santa.gd
+++ scenes/animated-santa.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
-@onready var _santa_standing = $AnimatedSprite2D
+@onready var _santa = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_santa_standing.play()
+	_santa.play("stand")
+
+func _process(_delta):
+	if Input.is_key_pressed(KEY_SPACE):
+		_santa.play("chop")
+	else:
+		_santa.play("stand")
